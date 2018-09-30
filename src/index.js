3c61d3c43cf38c60ef6b0e3d359f8ef87e3fabd0
Aliaksei Navamlinets <alex.novomlinets@gmail.com>
2018-09-30 10:51:22 +0300

    feat: implement feature according to the task#10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let hashTable = {},
+      count = 1,
+      countMax = 1;
+
+  if (array.length === 0) return 0;
+  if (array.length === 1) return 1;
+
+  for (let i = 0, len = array.length; i < len; i++ ) {
+    if(!hashTable[array[i]]) hashTable[array[i]] = 1;
+  }
+
+  for (let key in hashTable) {
+    if (hashTable[key] && hashTable[(parseInt(key) + 1 )]) count++;
+    else {
+      if (count >= countMax) countMax = count;
+      count = 1;
+    }
+  }
+
+  return countMax;
 }
